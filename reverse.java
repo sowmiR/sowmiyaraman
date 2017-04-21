f4ee4bef0b567d85a5bf8b37f432abac2292e4ce
sowmiR <sowmiyaraman16@gmail.com>
2017-04-21 15:35:52 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,17 @@
+package nbvn;
+
+public class reverse {
+public static void main(String[]args){
+int number=1234;
+int reverse =0;
+int temp=0;
+while(number>0){
+	temp=number%10;
+	reversed number=reversed number*10+temp;
+	number=number/10;
+	
+}
+System.out.println("reversed number is":+reversed number);
+
+}
+}
